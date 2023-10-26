255de80cdb294cc058f12c627d4a4531e0f22618
Michael Mote <michael.mote1993@gmail.com>
2023-10-26 13:29:47 -0500

    caeser cipher

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 const open = document.querySelectorAll('[data-open-button]')
 const close = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
+const submit = document.getElementById('submit')
+const testText = document.getElementById('test')
+const testBtn = document.getElementById('testBtn')
 overlay.addEventListener('click', () => {
     const modals = document.querySelectorAll('.modal.active')
     modals.forEach(modal => {
@@ -33,4 +36,18 @@ function closeModal(modal) {
     if (modal == null) return
     modal.classList.remove('active')
     overlay.classList.remove('active')
+}
+
+function caeserCipher(message, key) {
+    let result = ''
+    for (let letter of message) {
+        if (letter.toUpperCase(letter)) {
+            let char = String.fromCharCode((letter.charCodeAt(0) + key - 65) % 26 + 65)
+            result += char
+        } else {
+            let char = String.fromCharCode((char.charCodeAt(0) + key - 97) % 26 + 97)
+            result += char
+        }    
+    }
+    return result
 }
