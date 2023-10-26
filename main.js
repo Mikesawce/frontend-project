18e0e1fd0bb6f0799d632eba2db4507473bf757b
Michael Mote <michael.mote1993@gmail.com>
2023-10-26 16:01:08 -0500

    Caeser Cipher works on email send

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 const open = document.querySelectorAll('[data-open-button]')
 const close = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
+
 overlay.addEventListener('click', () => {
     const modals = document.querySelectorAll('.modal.active')
     modals.forEach((modal) => {
@@ -36,24 +37,31 @@ function closeModal(modal) {
     overlay.classList.remove('active')
 }
 
-let body = document.getElementById('message').value
+// encrypt.forEach((button) => {
+//     button.addEventListener('click', () => {
+//         const body = document.getElementById('message').value
+//         caeserCipher(body)
+//     })
+// })
 
-function caeserCipher(message) {
-    let result = ''
+function caesarCipher(message, shift) {
+    let result = '';
     for (let letter of message) {
-        if (letter.toUpperCase(letter)) {
-            let char = String.fromCharCode((letter.charCodeAt(0) + 4 - 65) % 26 + 65)
-            result += char
+        let charCode = letter.charCodeAt(0);
+        if (letter >= 'A' && letter <= 'Z') {
+            result += String.fromCharCode(((charCode - 65 + shift) % 26) + 65);
+        } else if (letter >= 'a' && letter <= 'z') {
+            result += String.fromCharCode(((charCode - 97 + shift) % 26) + 97);
         } else {
-            let char = String.fromCharCode((char.charCodeAt(0) + 4 - 97) % 26 + 97)
-            result += char
+            result += letter;
         }
     }
-    return result
+    return result;
 }
 
-
-function emailSend(body) {
+function emailSend() {
+    let body = document.getElementById('message').value
+    let messageBody = caesarCipher(body, 4)
     Email.send({
         Host: 'smtp.elasticemail.com',
         Username: 'michael.mote1993@gmail.com',
@@ -61,6 +69,6 @@ function emailSend(body) {
         To: 'michael.mote1993@gmail.com',
         From: 'michael.mote1993@gmail.com',
         Subject: 'THE NUMBERS MASON, WHAT DO THEY MEAN?',
-        Body: body
+        Body: messageBody
     }).then((message) => alert(message))
 }
